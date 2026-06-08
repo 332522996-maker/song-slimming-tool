60ba37168405dc85be3a83d269dfe1fbd695be9d
332522996-maker <332522996@qq.com>
2026-06-08 14:15:15 +0700

    Update firebase.ts

--- Comprehensive Health Dashboard/src/firebase.ts
+++ Comprehensive Health Dashboard/src/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// 这里已经完全替换成了你截图里的真实配置！
 const firebaseConfig = {
   apiKey: "AIzaSyAmE6CCF_9KAVTP4xfAYcwKB_5vvK0tlc8",
   authDomain: "song-slimming-tool.firebaseapp.com",
   projectId: "song-slimming-tool",
   storageBucket: "song-slimming-tool.firebasestorage.app",
   messagingSenderId: "1001172503763",
-  appId: "1:1001172503763:web:4507c918adbc4010ae5826"
+  appId: "1:1001172503763:web:b3b69c39b6984185ae5826" // 这里是你全新的应用 ID
 };
 
 const app = initializeApp(firebaseConfig);
